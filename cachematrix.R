7fa6b3f68486efdcab3f4614b3664b45c9c7016b
cbweaver <33897238+cbweaver@users.noreply.github.com>
2018-01-21 11:56:13 -0600

    Add files via upload
    Completed Programming Assignment 2 cweaver

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cbweaver 01.21.18
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing a function to
+##   set the value of a matrix
+##   get the value of a matrix
+##   set the value of the inverse of the matrix
+##   get the value of the inverse of the matrix
+##   the value of the matrix passed into the function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  xmatrix = x
+  i <- NULL
+  
+  setcacheddata <- function(y = matrix()) {
+    x <<- y
+    i <<- NULL
+  }
+  getcacheddata <- function() {
+    x
+  }
+  setcachedinverse <- function(inverse = matrix()) {
+    i <<- inverse
+  }
+  getcachedinverse <- function() {
+    i
+  }
+  ## create a list with the new functions and value of x
+  list(setcacheddata = setcacheddata, 
+       getcacheddata = getcacheddata, 
+       setcachedinverse = setcachedinverse,
+       getcachedinverse = getcachedinverse, orig = xmatrix)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special
+## "matrix" regurned by makeCacheMatrix. If the inverse 
+## already been calculated and the matrix has not changed
+## then return the inverse from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  i <- x$getcachedinverse()
+  data1 <- x$getcacheddata()
+  data2 <- x[[5]]
+     if(!is.null(i)) {
+      if (identical(data1,data2)) {
+    message("getting cached data")
+    return(i)
+      }
+    }
+  #cacheddata <- x$getcacheddata()
+  #message(cacheddata)
+  i <- solve(data1)
+  x$setcachedinverse(i)
+  i
+  
 }
+ 
+
